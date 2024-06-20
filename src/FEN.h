5dc2b8e0136e1eb087c6f3e74e45f45db5a23d03
AlejandroSuero <asuerome@gmail.com>
2024-06-20 12:53:05 +0200

    feat: add `FEN` notation parser

---
+++ src/FEN.h
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+namespace FEN
+{
+  std::vector<std::string> parse(const std::string &fen);
+  std::vector<std::string> get_initial_positions();
+  std::string get_positions(const std::vector<std::string> &positions);
+};
