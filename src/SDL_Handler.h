bda9ed474545bd0d0e8b2d04a04ee8ad51df98aa
AlejandroSuero <asuerome@gmail.com>
2024-06-20 12:53:42 +0200

    feat: load image as texture and calculate piece pos based on FEN

--- src/SDL_Handler.h
+++ src/SDL_Handler.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include <SDL.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2_image/SDL_image.h>
 
 class SDL_Handler
 {
@@ -12,20 +14,26 @@ public:
   void render_background();
   void render_board();
   void handle_events();
+  void render_texture(SDL_Texture *texture, int t_x, int t_y, int x, int y);
+  SDL_Texture *load_texture(const char *path);
 
 public:
   bool handling_events = true;
+  const char *PIECES_PATH = "../resources/Pieces.png";
+
+private:
+  const int m_SCREEN_WIDTH = 900;
+  const int m_SCREEN_HEIGHT = 900;
+  const int m_BOARD_SIZE = 720;
+  const int m_CELL_SIZE = m_BOARD_SIZE / 8;
 
 private:
   SDL_Window *m_window;
   SDL_Renderer *m_renderer;
-  SDL_Event m_event;
-  const int SCREEN_WIDTH = 800;
-  const int SCREEN_HEIGHT = 800;
-  const int BOARD_WIDTH = 640;
-  const int BOARD_HEIGHT = 640;
-  int m_win_width = SCREEN_WIDTH;
-  int m_win_height = SCREEN_HEIGHT;
+  SDL_Event m_event{};
+  SDL_Texture *m_texture{};
+  int m_win_width = m_SCREEN_WIDTH;
+  int m_win_height = m_SCREEN_HEIGHT;
 #if defined(__APPLE__) || defined(__MACH__)
   Uint32 m_time_left();
   Uint32 m_next_time = SDL_GetTicks() + 60;
